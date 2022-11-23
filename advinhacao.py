7d4fb3013a1e0dd07910cc3eca4d95298fe359c9
richard lirio <richardlirio@hotmail.com>
2022-11-23 20:13:27 -0300

    adicionando condicoes de possiveis respostas

--- advinhacao.py
+++ advinhacao.py
@@ -1,5 +1,17 @@
 print('***********************\nBem vindo ao jogo de adivinhação!\n***********************')
 numero_secreto = 42
-int(input('digite um numero:'))
 
+chute = int(input('digite um numero:'))
+
+maior = chute>numero_secreto
+menor = chute<numero_secreto
+acertou = chute == numero_secreto
+
+if(acertou):
+    print('Parabés voce acertou!')
+else:
+    if(maior):
+        print('Meu número é menor que {}!'.format(chute))
+    elif(menor):
+        print('Meu número é maior que {}!'.format(chute))
 
