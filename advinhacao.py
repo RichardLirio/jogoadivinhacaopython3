86c3ef7b6f3b74a4e14fbaf888e35b87954b41af
richard lirio <richardlirio@hotmail.com>
2022-11-23 21:06:06 -0300

    limitando o numero a ser digitado como chute!

--- advinhacao.py
+++ advinhacao.py
@@ -5,20 +5,22 @@ total_de_tentativas = 3
 for rodada in range(1,total_de_tentativas+1):
 
     print('Rodada {0}/{1}'.format(rodada,total_de_tentativas))
-    chute = int(input('digite um numero:'))
+    chute = int(input('digite um numero entre 0 e 100:\n'))
     maior = chute > numero_secreto
     menor = chute < numero_secreto
     acertou = chute == numero_secreto
-    
-    if(acertou):
-        print('Parabés voce acertou!')
-        break
-        
+    if(chute < 1 or chute > 100):
+        print('Voce deve digitar um numero entre 1 e 100!')
     else:
-        if(maior):
-            print('Meu número é menor que {}'.format(chute))
-        elif(menor):
-            print('Meu número é maior que {}'.format(chute))
+        if(acertou):
+            print('Parabés voce acertou!')
+            break
+            
+        else:
+            if(maior):
+                print('Meu número é menor que {}'.format(chute))
+            elif(menor):
+                print('Meu número é maior que {}'.format(chute))
         
     
 
